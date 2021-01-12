fcd2dc8ea9dbb05363a206d4c6c0065f2be1455b
shmelevaann <shmelevaann@gmail.com>
2021-01-12 22:03:47 +0300

    origin

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,6 @@
+create table products (id bigserial primary key, title varchar(255), price int);
+insert into products (title, price) values
+('Apple', 10),
+('Pear', 15),
+('Banana', 20),
+('Orange', 25);
