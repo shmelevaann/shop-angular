8dac4eefc64a7c3cb2a3dc532bfd56060dc73d51
shmelevaann <shmelevaann@gmail.com>
2021-02-18 18:24:52 +0300

    aop statistic re-written front

--- src/main/java/ru/chiffa/statistic/ControllerTimeAspect.java
+++ src/main/java/ru/chiffa/statistic/ControllerTimeAspect.java
@@ -1,20 +1,17 @@
 package ru.chiffa.statistic;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Aspect
-@Component
 public class ControllerTimeAspect {
     private final Map<String, Long> controllerTimeCounter = new ConcurrentHashMap<>();
 
-    @Around("execution(* * ru.chiffa.controllers.*(..)")
+    @Around("execution(* ru.chiffa.controllers..*(..))")
     public Object countTimeAroundMethod(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         String controllerName = proceedingJoinPoint.getSignature().getDeclaringTypeName();
 
