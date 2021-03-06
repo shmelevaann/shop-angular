8e951a876a2e2a816824e8f03b8196cd5e386dd7
shmelevaann <shmelevaann@gmail.com>
2021-03-06 15:48:24 +0300

    aop statistic

--- src/main/java/ru/chiffa/statistic/ControllerTimeAspect.java
+++ src/main/java/ru/chiffa/statistic/ControllerTimeAspect.java
@@ -3,11 +3,13 @@ package ru.chiffa.statistic;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Aspect
+@Component
 public class ControllerTimeAspect {
     private final Map<String, Long> controllerTimeCounter = new ConcurrentHashMap<>();
 
